dc6a46519e19ebcefc06c4dd0369a8e833525515
Kenn Sugiyama <kenn@kbsugiyama.com>
2017-10-28 19:35:22 -0700

    Create call to weather API and parse returned data.

--- script.js
+++ script.js
@@ -1,3 +1,94 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  var weather = {};
+  getGeoLocation();
+  console.log(weather)
+
+  function getGeoLocation() {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getWeather);
+    } else {
+      console.log("Geolocation is not being supported on this browser");
+    }
+  }
+
+  function getWeather(location){
+    let url = "https://fcc-weather-api.glitch.me/api/current?lon=" +
+          location.coords.longitude + "&lat=" +
+          location.coords.latitude
+    $.ajax({
+      url: url,
+      jsonp: "json",
+      dataType: "json"
+    })
+    .done(parseJSON)
+    .fail(handleError)
+
+    function parseJSON(JSON) {
+      let weatherPic = JSON.weather[0].icon
+      let locationName = JSON.name
+      let weather = JSON.weather[0].main
+      displayWeather(weatherPic, locationName, weather)
+    }
+
+    function handleError(jqxhr, textStatus, err) {
+      console.log("Request Failed: " + textStatus + ", " + err);
+    }//handleError
+  }//get Weather
+
+  function displayWeather(weatherPic,locationName,weather){
+    $("#intro-header")
+    console.log(weatherPic,locationName,weather)
+  }
 
 })
+
+
+
+
+
+
+
+
+ // let getQuote = function(){
+ //    $.ajax({
+ //        url: "https://api.forismatic.com/api/1.0/",
+ //        jsonp: "jsonp",
+ //        dataType: "jsonp",
+ //        data: {
+ //          method: "getQuote",
+ //          lang: "en",
+ //          format: "jsonp"
+ //        }
+ //      })
+ //      .done(parseJSON)
+ //      .fail(handleError);
+
+ //    function parseJSON(JSON) {
+ //      let quoteText = JSON.quoteText;
+ //      let author = "- "
+ //      author += JSON.quoteAuthor;
+ //      displayQuote(quoteText, author);
+ //    }
+
+ //    function handleError(jqxhr, textStatus, err) {
+ //      console.log("Request Failed: " + textStatus + ", " + err);
+ //    }
+ //  }// getQuote
+
+
+
+
+// <script>
+// var x = document.getElementById("demo");
+// function getLocation() {
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(showPosition);
+//     } else {
+//         x.innerHTML = "Geolocation is not supported by this browser.";
+//     }
+// }
+// function showPosition(position) {
+//     x.innerHTML = "Latitude: " + position.coords.latitude +
+//     "<br>Longitude: " + position.coords.longitude;
+// }
+// </script>
